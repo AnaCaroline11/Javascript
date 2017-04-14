2eab7b2aa17b2660dd3eee3abe86d66f3a7bb879
AnaCaroline11 <carolinelucio08@gmail.com>
2017-04-14 09:07:23 -0300

    Create exemplo 14_1.js

---
+++ exemplo 14_1.js
@@ -0,0 +1,9 @@
+function evento_entrar(){
+alert('Seja bem vindo');
+}
+function evento_sair(){
+alert('Até a proxima visita');
+}
+function evento_clique(){
+winddow.document.getElementById('id2').style.color='red';
+}
